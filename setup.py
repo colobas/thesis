530d7249c92661b366861a29d3afdf2e607ff58c
colobas <mail@gpir.es>
2019-03-23 23:57:02 -0700

    restructuring

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='guilhermepires-thesis',
+      version='0.0',
+      description='Guilherme Pires MSc Thesis',
+      author='Guilherme Pires',
+      author_email='mail@gpir.es',
+      license='MIT',
+      packages=['ts_deep_gmm'],
+      zip_safe=False)
