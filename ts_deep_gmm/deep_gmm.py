a1dd3a2d7f023cb106af54ddaff28087ac2007e1
colobas <mail@gpir.es>
2019-04-07 14:45:21 -0700

    fixing stuff

--- ts_deep_gmm/deep_gmm.py
+++ ts_deep_gmm/deep_gmm.py
@@ -11,11 +11,13 @@ from torch.autograd import gradcheck
 from torch.distributions.multivariate_normal import _batch_mahalanobis, _batch_diag, _batch_mv
 from torch.distributions import RelaxedOneHotCategorical, MultivariateNormal, Uniform
 
+from tqdm import trange
+
 from ts_deep_gmm.utils.bad_grad_viz import register_hooks
 
 _DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
 
-def mv_gaussian_log_prob(value, μ, Σ, event_shape, diag_mode=False):
+def mv_gaussian_log_prob(value, μ, Σ, event_shape, diag_mode=False, debug=False):
     """
     Adapted from https://github.com/pytorch/pytorch/blob/master/torch/distributions/multivariate_normal.py
     """
@@ -29,15 +31,15 @@ def mv_gaussian_log_prob(value, μ, Σ, event_shape, diag_mode=False):
         M = _batch_mahalanobis(scale_tril, diff)
         half_log_det = _batch_diag(scale_tril).log().sum(-1)
 
-#    if M.mean().abs() > 1000:
-    if True:
-        #print(f"M.mean(): {M.mean()}")
-        #print(f"value: {value}")
-        #print(f"mu: {μ}")
-        #print(f"half_log_det: {half_log_det}")
-        #print(f"diags: {diags}")
+    if debug:
+        print("###################################################################################")
+        print(f"M.mean(): {M.mean()}")
+        print(f"value: {value}")
+        print(f"mu: {μ}")
+        print(f"half_log_det: {half_log_det}")
         print(f"diff**2: {diff**2}")
-        print(f"diags**2: {diags**2}")
+        if diag_mode:
+            print(f"diags**2: {diags**2}")
 
     return -0.5 * (event_shape * math.log(2 * math.pi) + M) - half_log_det
 
@@ -80,7 +82,9 @@ class DeepGMM(nn.Module):
         ]))
 
 
-        self.ϕ_gmm_πs = nn.Parameter(F.softmax(torch.randn(n_clusters), dim=0))
+        # store in logits form because it's unconstrained,
+        # then use log_softmax to convert to log probabilities
+        self.ϕ_gmm_logits_πs = nn.Parameter(torch.randn(n_clusters))
         ##########################################################
 
         ################## Generative parameters #################
@@ -102,8 +106,9 @@ class DeepGMM(nn.Module):
             for _ in range(n_clusters)
         ]))
 
-
-        self.θ_gmm_πs = nn.Parameter(F.softmax(torch.randn(n_clusters), dim=0))
+        # store in logits form because it's unconstrained,
+        # then use log_softmax to convert to log probabilities
+        self.θ_gmm_logits_πs = nn.Parameter(torch.randn(n_clusters))
         ##########################################################
 
     def _Σ(self, factor, diag, diag_size):
@@ -134,7 +139,11 @@ class DeepGMM(nn.Module):
     def θ_gmm_Σs(self):
         return self._Σs(self.θ_gmm_Σ_factors, self.θ_gmm_Σ_diags)
 
-
+    def forward(Y):
+        """
+        Given data points Y, infer their latent X and Z
+        """
+        raise NotImplemented
 
     def _fit_step(self, Y, temperature, n_samples=10):
         ϕ_enc_μ, ϕ_enc_Σ = self.encoder(Y)
@@ -149,18 +158,19 @@ class DeepGMM(nn.Module):
         # to deal with mv_gaussian_log_prob per cluster
         # can I somehow map the loop iteration?
         # EDIT: I think this is doable with flatten/view !!!
+
+        ϕ_gmm_log_πs = F.log_softmax(self.ϕ_gmm_logits_πs, dim=0)
+
+        print(f"ϕ_gmm_log_πs: {ϕ_gmm_log_πs}")
+
         for k in range(self.n_clusters):
             ϕ_gmm_Σ_k = self.ϕ_gmm_Σ_k(k)
 
-            try:
-                z_logits[:,k] = (self.ϕ_gmm_πs[k].log() + 
-                                 mv_gaussian_log_prob(value=ϕ_enc_μ, μ=self.ϕ_gmm_μs[k],
-                                                      Σ=ϕ_enc_Σ+ϕ_gmm_Σ_k,
-                                                      event_shape=self.x_dim,
-                                                      diag_mode=self.diag_mode))
-            except:
-                print(ϕ_enc_Σ)
-                assert False
+            z_logits[:,k] = (ϕ_gmm_log_πs[k] + 
+                             mv_gaussian_log_prob(value=ϕ_enc_μ, μ=self.ϕ_gmm_μs[k],
+                                                  Σ=ϕ_enc_Σ+ϕ_gmm_Σ_k,
+                                                  event_shape=self.x_dim,
+                                                  diag_mode=self.diag_mode))
 
             # TODO: check if I'm being the smartest possible about these inverses
             # Repeated inverses? Also confirm the batch operations make sense
@@ -179,7 +189,12 @@ class DeepGMM(nn.Module):
         # this results in a (N, n_samples, n_clusters) tensor: N*n_samples "pseudo one-hot" vectors
         #z_samples = RelaxedOneHotCategoricalStraightThrough(temperature, logits=z_logits).rsample(n_samples)
 
-        z_samples = RelaxedOneHotCategorical(temperature, logits=z_logits).rsample((n_samples,)).transpose(0, 1)
+        z_log_probs = F.log_softmax(z_logits, dim=-1)
+        print(f"z_logits: {z_logits}")
+        print(f"z_log_probs: {z_log_probs}")
+
+
+        z_samples = RelaxedOneHotCategorical(temperature, probs=z_log_probs.exp()).rsample((n_samples,)).transpose(0, 1)
 
         # now use each "pseudo one-hot" vector to select which ϕ_tilde to use
         _μ_tilde = torch.einsum("bsk,bkd->bsd", z_samples, μ_tilde)
@@ -205,50 +220,65 @@ class DeepGMM(nn.Module):
         #x_samples = x_samples.view(100, 5, 2)
 
         # decoder NN part of the loss
-        print("############################### decoder NN gaussian log prob")
-        obj = mv_gaussian_log_prob(Y.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
+        #obj = mv_gaussian_log_prob(Y.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
+        loss1 = mv_gaussian_log_prob(Y.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
                                    μ_y,
                                    Σ_y,
                                    self.y_dim,
                                    self.diag_mode)
 
         # encoder NN part of the loss
-        print("############################### encoder NN gaussian log prob")
-        obj = obj - mv_gaussian_log_prob(x_samples,
+        #obj = obj - mv_gaussian_log_prob(x_samples,
+        loss2 = - mv_gaussian_log_prob(x_samples,
                                          ϕ_enc_μ.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
                                          ϕ_enc_Σ.unsqueeze(1).repeat(1, n_samples, 1, 1).flatten(0, 1),
                                          self.x_dim,
                                          self.diag_mode)
 
         # encoder GMM part of the loss
-        print("############################### encoder GMM gaussian log prob")
-        obj = obj + mv_gaussian_log_prob(x_samples, _θ_gmm_μ.flatten(0, 1), _θ_gmm_Σ.flatten(0, 1), self.x_dim, self.diag_mode)
-        obj = obj + (self.θ_gmm_πs.log() * z_samples.flatten(0, 1)).sum(dim=1) #IT'S BREAKING HERE
+        #obj = obj + mv_gaussian_log_prob(x_samples,
+        loss3 = (mv_gaussian_log_prob(x_samples,
+                                         _θ_gmm_μ.flatten(0, 1),
+                                         _θ_gmm_Σ.flatten(0, 1),
+                                         self.x_dim,
+                                         self.diag_mode) +
+                 (F.log_softmax(self.θ_gmm_logits_πs, dim=0) *
+                     z_samples.flatten(0, 1)).sum(dim=1))
 
         # decoder GMM part of the loss
-        print("############################### decoder GMM gaussian log prob")
-        obj = obj - mv_gaussian_log_prob(x_samples, _ϕ_gmm_μ.flatten(0, 1), _ϕ_gmm_Σ.flatten(0, 1), self.x_dim, self.diag_mode)
-        obj = obj - (z_logits.unsqueeze(1) * z_samples).flatten(0, 1).sum(dim=1)
+        #obj = obj - mv_gaussian_log_prob(x_samples,
+
+        loss4 = -(mv_gaussian_log_prob(x_samples,
+                                         _ϕ_gmm_μ.flatten(0, 1),
+                                         _ϕ_gmm_Σ.flatten(0, 1),
+                                         self.x_dim,
+                                         self.diag_mode) +
+                  (z_log_probs.unsqueeze(1) * z_samples).flatten(0, 1).sum(dim=1))
 
 
-        print(f"obj.sum() = {obj.sum()}")
+        loss5 = z_log_probs.exp().sum(dim=1).log().sum()
 
-        obj = obj.sum()/float(n_samples)
 
-        #return -(obj + z_logits.exp().sum(dim=1).log().sum())
-        return (obj + z_logits.exp().sum(dim=1).log().sum())
+        #for i, loss in enumerate([loss1, loss2, loss3, loss4, loss5]):
+        #    print(f"loss_{i+1}: {loss}")
+
+        # what's inside parens is the ELBO, and we want to maximize it,
+        # hence minimize its reciprocal
+        res =  -((loss1 + loss2 + loss3 + loss4).sum()/n_samples + loss5)
+        #print(f"res: {res}")
+        return res
 
 
     def fit(self, Y, temperature_schedule=None, n_epochs=1, bs=100, opt=None,
-            n_samples=10, debug=False):
+            n_samples=10, verbose=False, debug=False):
 
         # TODO: implement default values
 
         if temperature_schedule is None:
             # default temperature schedule, adapted from https://arxiv.org/pdf/1611.01144.pdf
             # TODO: r and M should be hyperparams
-            r = Uniform(1e-5, 1e-5).sample()
-            M = torch.randint(500, 1001, (1,)).item()
+            r = Uniform(1e-5, 1e-4).sample()
+            M = torch.randint(bs//2, bs, (1,)).item()
             temperature = 1
             def temperature_schedule(epoch, i, bs):
                 t = (epoch * bs + i)
@@ -263,8 +293,15 @@ class DeepGMM(nn.Module):
 
         losses = []
 
-        for epoch in range(n_epochs):
-            for i in range((len(Y) - 1) // bs + 1):
+        if verbose:
+            epochs = trange(n_epochs, desc="epoch")
+            batches = trange((len(Y) - 1) // bs + 1, desc="batch")
+        else:
+            epochs = range(n_epochs)
+            batches = range((len(Y) - 1) // bs + 1)
+
+        for epoch in epochs:
+            for i in batches:
                 start_i = i * bs
                 end_i = start_i + bs
                 yb = Y[start_i:end_i]
@@ -273,18 +310,25 @@ class DeepGMM(nn.Module):
 
                 loss = self._fit_step(yb, temperature, n_samples)
                 losses.append((epoch, i, loss.item()))
-                if debug != 0:
-                    get_dot = register_hooks(loss)
+                #if debug != 0:
+                    #get_dot = register_hooks(loss)
 
                 loss.backward()
                 if debug != 0:
+                    debug_str = ""
+                    print_me = False
                     if (epoch * bs + i) < debug:
-                        print(f"################### debug iter {(epoch * bs + i)} #####################")
+                        debug_str += (f"################### debug iter {(epoch * bs + i)} #####################\n")
                         for n, p in self.named_parameters():
-                            print(f"{n}: {p.grad.mean()}")
-                        print("\n")
-                        dot = get_dot() 
-                        dot.render(f"iter_{i}", format="pdf")
+                            grad_mean = p.grad.mean()
+                            debug_str += (f"{n}: {p.grad.mean()}\n")
+                            if grad_mean != grad_mean:
+                                print_me = True
+                        debug_str += "\n"
+                        #dot = get_dot() 
+                        #dot.render(f"iter_{i}", format="pdf")
+                        if print_me:
+                            print(debug_str)
                     else:
                         assert False
 
