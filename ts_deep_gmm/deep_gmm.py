5e25f39a008d28eef791e41a5405b9e7d441f8fb
colobas <mail@gpir.es>
2019-04-08 11:09:40 -0700

    started moving everything to diag mode

--- ts_deep_gmm/deep_gmm.py
+++ ts_deep_gmm/deep_gmm.py
@@ -17,19 +17,14 @@ from ts_deep_gmm.utils.bad_grad_viz import register_hooks
 
 _DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
 
-def mv_gaussian_log_prob(value, μ, Σ, event_shape, diag_mode=False, debug=False):
+def mv_gaussian_log_prob(value, μ, Σ_diags, event_shape, debug=False):
     """
     Adapted from https://github.com/pytorch/pytorch/blob/master/torch/distributions/multivariate_normal.py
+    FOR DIAGONAL COV MATRICES ONLY
     """
     diff = value - μ
-    if diag_mode:
-        diags = _batch_diag(Σ)
-        M = ((diff**2) / diags**2).sum(-1)
-        half_log_det = diags.log().sum(-1)
-    else:
-        scale_tril = torch.cholesky(Σ)
-        M = _batch_mahalanobis(scale_tril, diff)
-        half_log_det = _batch_diag(scale_tril).log().sum(-1)
+    M = ((diff**2) / Σ_diags**2).sum(-1)
+    half_log_det = Σ_diags.log().sum(-1)
 
     if debug:
         print("###################################################################################")
@@ -38,8 +33,7 @@ def mv_gaussian_log_prob(value, μ, Σ, event_shape, diag_mode=False, debug=Fals
         print(f"mu: {μ}")
         print(f"half_log_det: {half_log_det}")
         print(f"diff**2: {diff**2}")
-        if diag_mode:
-            print(f"diags**2: {diags**2}")
+        print(f"diags**2: {diags**2}")
 
     return -0.5 * (event_shape * math.log(2 * math.pi) + M) - half_log_det
 
@@ -65,109 +59,38 @@ class DeepGMM(nn.Module):
         ################# Recognition parameters #################
         self.encoder = encoder # observ. -> latent (params)
 
-        self.ϕ_gmm_μs = nn.Parameter(torch.stack([
+        self.ϕ_gmm_μs = torch.stack([
             torch.randn(x_dim)
             for _ in range(n_clusters)
-        ]))
+        ])
 
-        # Σ = Σ_factor @ Σ_factor.t() + Σ_diag
-        self.ϕ_gmm_Σ_sqrt_diags = nn.Parameter(torch.stack([
+        self.ϕ_gmm_Σ_diags = torch.stack([
             torch.randn(x_dim)
             for _ in range(n_clusters)
-        ]))
-
-        if not self.diag_mode:
-            self.ϕ_gmm_Σ_factors = nn.Parameter(torch.stack([
-                torch.randn(x_dim)
-                for _ in range(n_clusters)
-            ]))
-        else:
-            self.ϕ_gmm_Σ_factors = [None for _ in range(self.n_clusters)]
-
+        ])
 
         # store in logits form because it's unconstrained,
         # then use log_softmax to convert to log probabilities
-        self.ϕ_gmm_logits_πs = nn.Parameter(torch.randn(n_clusters))
+        self.ϕ_gmm_logits_πs = torch.randn(n_clusters)
         ##########################################################
 
         ################## Generative parameters #################
         self.decoder = decoder # latent var -> observ (params)
 
-        self.θ_gmm_μs = nn.Parameter(torch.stack([
+        self.θ_gmm_μs = torch.stack([
             torch.randn(y_dim)
             for _ in range(n_clusters)
-        ]))
+        ])
 
-        # Σ = Σ_factor @ Σ_factor.t() + Σ_diag
-        self.θ_gmm_Σ_sqrt_diags = nn.Parameter(torch.stack([
+        self.θ_gmm_Σ_diags = torch.stack([
             torch.randn(y_dim)
             for _ in range(n_clusters)
-        ]))
-
-        if not self.diag_mode:
-            self.θ_gmm_Σ_factors = nn.Parameter(torch.stack([
-                torch.randn(y_dim)
-                for _ in range(n_clusters)
-            ]))
-        else:
-            self.θ_gmm_Σ_factors = [None for _ in range(self.n_clusters)]
+        ])
 
         # store in logits form because it's unconstrained,
         # then use log_softmax to convert to log probabilities
-        self.θ_gmm_logits_πs = nn.Parameter(torch.randn(n_clusters))
+        self.θ_gmm_logits_πs = torch.randn(n_clusters)
         ##########################################################
-
-    def ϕ_gmm_Σ_diag_k(self, k):
-        #return self.ϕ_gmm_Σ_sqrt_diags[k]**2 + self.reg
-        return F.sigmoid(self.ϕ_gmm_Σ_sqrt_diags[k]) + self.reg
-
-    @property
-    def ϕ_gmm_Σ_diags(self):
-        #return self.ϕ_gmm_Σ_sqrt_diags**2 + self.reg
-        return F.sigmoid(self.ϕ_gmm_Σ_sqrt_diags) + self.reg
-
-    def θ_gmm_Σ_diag_k(self, k):
-        #return self.θ_gmm_Σ_sqrt_diags[k]**2 + self.reg
-        return F.sigmoid(self.θ_gmm_Σ_sqrt_diags[k])+ self.reg
-
-    @property
-    def θ_gmm_Σ_diags(self):
-        #return self.θ_gmm_Σ_sqrt_diags**2 + self.reg
-        return F.sigmoid(self.θ_gmm_Σ_sqrt_diags) + self.reg
-
-    def _Σ(self, factor, diag, diag_size):
-        diag_term = torch.eye(diag_size, device=_DEVICE)*diag
-        if not self.diag_mode:
-            factor = factor.unsqueeze(-1)
-            return diag_term + (factor @ factor.t())
-        else:
-            return diag_term
-
-    def ϕ_gmm_Σ_k(self, k):
-        Σ_diag = self.ϕ_gmm_Σ_diag_k(k)
-        Σ_factor = self.ϕ_gmm_Σ_factors[k]
-        return self._Σ(Σ_factor, Σ_diag, self.x_dim)
-
-    def θ_gmm_Σ(self, k):
-        Σ_diag = self.θ_gmm_Σ_diag_k(k)
-        Σ_factor = self.θ_gmm_Σ_factors[k]
-        return self._Σ(Σ_factor, Σ_diag, self.y_dim)
-
-    def _Σs(self, factors, diags):
-        diag_term = torch.diag_embed(diags + self.reg)
-        if not self.diag_mode:
-            factors = factors.unsqueeze(-1)
-            return (factors @ torch.transpose(factors, 1, 2)) + diat_term
-        else:
-            return diag_term
-
-    @property
-    def ϕ_gmm_Σs(self):
-        return self._Σs(self.ϕ_gmm_Σ_factors, self.ϕ_gmm_Σ_diags)
-
-    @property
-    def θ_gmm_Σs(self):
-        return self._Σs(self.θ_gmm_Σ_factors, self.θ_gmm_Σ_diags)
 
     def forward(Y):
         """
@@ -182,7 +105,8 @@ class DeepGMM(nn.Module):
         μ_tilde = torch.zeros((N, self.n_clusters, self.x_dim), device=_DEVICE)
         Σ_tilde = torch.zeros((N, self.n_clusters, self.x_dim, self.x_dim), device=_DEVICE)
 
-        inv_ϕ_enc_Σ = torch.inverse(ϕ_enc_Σ)
+        #inv_ϕ_enc_Σ = torch.inverse(ϕ_enc_Σ)
+        inv_ϕ_enc_Σ = 1/ϕ_enc_Σ
 
         # TODO: do this without the loop? I think the question is how
         # to deal with mv_gaussian_log_prob per cluster
@@ -192,52 +116,49 @@ class DeepGMM(nn.Module):
         ϕ_gmm_log_πs = F.log_softmax(self.ϕ_gmm_logits_πs, dim=0)
 
         for k in range(self.n_clusters):
-            ϕ_gmm_Σ_k = self.ϕ_gmm_Σ_k(k)
-
             aux = mv_gaussian_log_prob(value=ϕ_enc_μ, μ=self.ϕ_gmm_μs[k],
-                                       Σ=ϕ_enc_Σ+ϕ_gmm_Σ_k,
-                                       event_shape=self.x_dim,
-                                       diag_mode=self.diag_mode,
-                                       )
+                                       Σ_diags=ϕ_enc_Σ+ϕ_gmm_Σ[k],
+                                       event_shape=self.x_dim)
             z_logits[:,k] = (ϕ_gmm_log_πs[k] + aux)
 
             # TODO: check if I'm being the smartest possible about these inverses
             # Repeated inverses? Also confirm the batch operations make sense
-            inv_ϕ_gmm_Σ = torch.inverse(ϕ_gmm_Σ_k)
-            Σ_tilde[:,k,:,:] = torch.inverse(
+            #inv_ϕ_gmm_Σ = torch.inverse(ϕ_gmm_Σ[k])
+            inv_ϕ_gmm_Σ = 1/torch.inverse(ϕ_gmm_Σ[k])
+
+            #Σ_tilde[:,k,:,:] = torch.inverse(
+            Σ_tilde[:,k,:] = 1/(
                 inv_ϕ_enc_Σ + # one matrix per batch element (N, x_dim, x_dim)
                 inv_ϕ_gmm_Σ # one matrix per cluster (x_dim, x_dim)
             )
 
-            μ_tilde[:,k] = _batch_mv(Σ_tilde[:,k], (
-                (inv_ϕ_enc_Σ.unsqueeze(0) @ ϕ_enc_μ.unsqueeze(-1)).squeeze() +
-                (inv_ϕ_gmm_Σ @ self.ϕ_gmm_μs[k])
+            #μ_tilde[:,k] = _batch_mv(Σ_tilde[:,k], (
+            μ_tilde[:,k] = (Σ_tilde[:,k] * (
+                #(inv_ϕ_enc_Σ.unsqueeze(0) @ ϕ_enc_μ.unsqueeze(-1)).squeeze() +
+                #(inv_ϕ_gmm_Σ @ self.ϕ_gmm_μs[k])
+                (inv_ϕ_enc_Σ.unsqueeze(0) * ϕ_enc_μ).squeeze() +
+                (inv_ϕ_gmm_Σ * self.ϕ_gmm_μs[k])
             ))
 
         # for each batch element, sample n_samples
         # this results in a (N, n_samples, n_clusters) tensor: N*n_samples "pseudo one-hot" vectors
-        #z_samples = RelaxedOneHotCategoricalStraightThrough(temperature, logits=z_logits).rsample(n_samples)
 
         z_log_probs = F.log_softmax(z_logits, dim=-1)
-        #print(f"z_logits: {z_logits}")
-        #print(f"z_log_probs: {z_log_probs}")
-
-
         z_samples = RelaxedOneHotCategorical(temperature, probs=z_log_probs.exp()).rsample((n_samples,)).transpose(0, 1)
 
         # now use each "pseudo one-hot" vector to select which ϕ_tilde to use
         _μ_tilde = torch.einsum("bsk,bkd->bsd", z_samples, μ_tilde)
-        _Σ_tilde = torch.einsum("bsk,bkdf->bsdf", z_samples, Σ_tilde)
+        _Σ_tilde = torch.einsum("bsk,bkdf->bsd", z_samples, Σ_tilde)
 
         _θ_gmm_μ = torch.einsum("bsk,kd->bsd", z_samples, self.θ_gmm_μs)
-        _θ_gmm_Σ = torch.einsum("bsk,kdf->bsdf", z_samples, self.θ_gmm_Σs)
+        _θ_gmm_Σ = torch.einsum("bsk,kdf->bsd", z_samples, self.θ_gmm_Σs)
 
         _ϕ_gmm_μ = torch.einsum("bsk,kd->bsd", z_samples, self.ϕ_gmm_μs)
-        _ϕ_gmm_Σ = torch.einsum("bsk,kdf->bsdf", z_samples, self.ϕ_gmm_Σs)
+        _ϕ_gmm_Σ = torch.einsum("bsk,kdf->bsd", z_samples, self.ϕ_gmm_Σs)
 
         x_samples = (
             MultivariateNormal(loc=_μ_tilde.flatten(0, 1),
-                               covariance_matrix=_Σ_tilde.flatten(0, 1))
+                               covariance_matrix=torch.diag_embed(_Σ_tilde.flatten(0, 1)))
                 .rsample()
                 #.view(100, 5, 2)
         )
@@ -249,7 +170,6 @@ class DeepGMM(nn.Module):
         #x_samples = x_samples.view(100, 5, 2)
 
         # decoder NN part of the loss
-        #obj = mv_gaussian_log_prob(Y.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
         loss1 = mv_gaussian_log_prob(Y.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
                                    μ_y,
                                    Σ_y,
@@ -257,15 +177,13 @@ class DeepGMM(nn.Module):
                                    self.diag_mode)
 
         # encoder NN part of the loss
-        #obj = obj - mv_gaussian_log_prob(x_samples,
         loss2 = - mv_gaussian_log_prob(x_samples,
                                          ϕ_enc_μ.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
-                                         ϕ_enc_Σ.unsqueeze(1).repeat(1, n_samples, 1, 1).flatten(0, 1),
+                                         ϕ_enc_Σ.unsqueeze(1).repeat(1, n_samples, 1).flatten(0, 1),
                                          self.x_dim,
                                          self.diag_mode)
 
         # encoder GMM part of the loss
-        #obj = obj + mv_gaussian_log_prob(x_samples,
         loss3 = (mv_gaussian_log_prob(x_samples,
                                          _θ_gmm_μ.flatten(0, 1),
                                          _θ_gmm_Σ.flatten(0, 1),
@@ -275,15 +193,12 @@ class DeepGMM(nn.Module):
                      z_samples.flatten(0, 1)).sum(dim=1))
 
         # decoder GMM part of the loss
-        #obj = obj - mv_gaussian_log_prob(x_samples,
-
         loss4 = -(mv_gaussian_log_prob(x_samples,
                                          _ϕ_gmm_μ.flatten(0, 1),
                                          _ϕ_gmm_Σ.flatten(0, 1),
                                          self.x_dim,
                                          self.diag_mode) +
                   (z_log_probs.unsqueeze(1) * z_samples).flatten(0, 1).sum(dim=1))
-
 
         loss5 = z_log_probs.exp().sum(dim=1).log().sum()
 
